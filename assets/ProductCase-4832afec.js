bf0e7ce024730d9412f7573086a3b74cc01878e2
yes66395 <nos66395@gmail.com>
2023-02-16 11:51:16 +0800

    shopping-cart

---
+++ assets/ProductCase-4832afec.js
@@ -0,0 +1,1 @@
+import{_ as t,l as a,o as n,c as o,F as s,f as e}from"./index-0d61ff92.js";const c={name:"訂單元件開發當中",components:{},computed:{...a(["checkData"]),noCase(){return this.checkData.length===0}}},r=e("h1",null,"開發當中",-1),l=e("main",null,null,-1);function _(p,u,m,d,h,i){return n(),o(s,null,[r,l],64)}const k=t(c,[["render",_]]);export{k as default};
